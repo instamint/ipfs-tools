9c4e8eb1d12454ed8e757f0a06577e237cda80e8
Abdullah Al Mamun <mamunabms@yahoo.com>
2021-10-27 06:06:11 +0600

    Move all configurations to constants

--- main.py
+++ main.py
@@ -4,36 +4,42 @@ from pathlib import Path
 import requests
 from dotenv import load_dotenv
 import pandas
-
 import shutil
 
-output_directory = './output'
-wallet_address = "0x6FCA0F70BcC4a86786c79414F8E84BD542F7c250"
-if os.path.exists(output_directory):
-    shutil.rmtree(output_directory)
-    os.makedirs(output_directory)
-else:
-    os.makedirs(output_directory)
+# Base Configurations
+INPUT_FILE = 'test.csv'
+OUTPUT_FILE = 'output_test.csv'
+META_DATA_TEMPLATE_FILE = 'metadata_template.json'
+OUTPUT_DIRECTORY = './output/'
+WALLET_ADDRESS = "0x6FCA0F70BcC4a86786c79414F8E84BD542F7c250"
+INFURA_IPFS_API_ADD_URL = "https://ipfs.infura.io:5001/api/v0/add"
+IPFS_BASE_URL = "https://ipfs.io/ipfs/"
 
+if os.path.exists(OUTPUT_DIRECTORY):
+    shutil.rmtree(OUTPUT_DIRECTORY)
+    os.makedirs(OUTPUT_DIRECTORY)
+else:
+    os.makedirs(OUTPUT_DIRECTORY)
+
+# Read IPFS credentials from .env
 load_dotenv()
 try:
-    infura_ipfs_project_id= os.environ['INFURA_IPFS_PROJECT_ID']
-    infura_ipfs_project_secret= os.environ['INFURA_IPFS_PROJECT_SECRET']
+    INFURA_IPFS_PROJECT_ID= os.environ['INFURA_IPFS_PROJECT_ID']
+    INFURA_IPFS_PROJECT_SECRET= os.environ['INFURA_IPFS_PROJECT_SECRET']
 except KeyError as e:
     print(f"'{e.args[0]}'s  Environment variable is not properly set. Aborting")
     sys.exit(1)
 
-with open('metadata_template.json') as f:
+with open(META_DATA_TEMPLATE_FILE) as f:
   json_template = json.load(f)
 
-df = pandas.read_csv('input.csv')
+df = pandas.read_csv(INPUT_FILE)
 
 image_list = df.values.tolist()
-
 for image in image_list:
     image_url = image[4]
     image_name =urllib.parse.urlparse(image_url).path.rstrip('/').split('/')[-1]
-    image_path = "./output/" + image_name
+    image_path = OUTPUT_DIRECTORY + image_name
     r = requests.get(image_url)
     with open(image_path, "wb") as f:
         f.write(r.content)
@@ -41,27 +47,27 @@ for image in image_list:
     image_files = {
         image_name: open(image_path, "rb"),
     }
-    response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files=image_files, auth=(infura_ipfs_project_id,infura_ipfs_project_secret))
+    response = requests.post(INFURA_IPFS_API_ADD_URL, files=image_files, auth=(INFURA_IPFS_PROJECT_ID,INFURA_IPFS_PROJECT_SECRET))
     ipfs_image_data = response.json()
     image.append(ipfs_image_data["Hash"])
-    image.append("https://ipfs.io/ipfs/" + ipfs_image_data["Hash"])
+    image.append(IPFS_BASE_URL + ipfs_image_data["Hash"])
 
     metadata_json = copy.deepcopy(json_template)
     metadata_json["name"] = image_name
     metadata_json["description"] = image_name
     # 'name', 'description', 'external_url', 'instagram_username', 'instagram_share_url',
     # 'instagram_direct_link', 'image_ipfs_cid', 'contract_address', 'vintage_date', 'token_id'
-    metadata_json["external_url"] = "https://ipfs.io/ipfs/" + ipfs_image_data["Hash"]
+    metadata_json["external_url"] = IPFS_BASE_URL + ipfs_image_data["Hash"]
     metadata_json["instagram_username"] = image[1]
     metadata_json["instagram_share_url"] = image[2]
     metadata_json["instagram_direct_link"] = image[3]
     metadata_json["image_ipfs_cid"] = ipfs_image_data["Hash"]
-    metadata_json["contract_address"] = wallet_address
+    metadata_json["contract_address"] = WALLET_ADDRESS
     metadata_json["vintage_date"]=image[5]
     metadata_json["token_id"] = image[0]
 
     metadata_json_file_name = Path(image_path).stem + ".json"
-    metadata_json_file_path = "./output/" + metadata_json_file_name
+    metadata_json_file_path = OUTPUT_DIRECTORY + metadata_json_file_name
     with open(metadata_json_file_path, 'w') as fp:
         json.dump(metadata_json, fp, default=str)
     with open(metadata_json_file_path, 'w') as fp:
@@ -71,11 +77,11 @@ for image in image_list:
         metadata_json_file_name: open(metadata_json_file_path, "rb"),
     }
 
-    response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files=json_files, auth=(infura_ipfs_project_id,infura_ipfs_project_secret))
+    response = requests.post(INFURA_IPFS_API_ADD_URL, files=json_files, auth=(INFURA_IPFS_PROJECT_ID,INFURA_IPFS_PROJECT_SECRET))
     ipfs_metadata_data = response.json()
     image.append(ipfs_metadata_data["Hash"])
-    image.append("https://ipfs.io/ipfs/" + ipfs_metadata_data["Hash"])
-    image.append("0x6FCA0F70BcC4a86786c79414F8E84BD542F7c250")
+    image.append(IPFS_BASE_URL + ipfs_metadata_data["Hash"])
+    image.append(WALLET_ADDRESS)
     image.append(image[0])
 colnames = ['ID',
  'Instagram Username',
@@ -90,4 +96,4 @@ colnames = ['ID',
  'Contract Address',
  'Token ID']
 output_df = pandas.DataFrame(image_list, columns=colnames)
-output_df.to_csv('output.csv', index=False)
+output_df.to_csv(OUTPUT_FILE, index=False)
