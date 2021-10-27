99b676c0e5c7d22abac6428187cc37e722b945c6
Abdullah Al Mamun <mamunabms@yahoo.com>
2021-10-27 07:30:26 +0600

    Use image_list with dictionary values for better readability and maintainability

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ WALLET_ADDRESS = "0x6FCA0F70BcC4a86786c79414F8E84BD542F7c250"
 INFURA_IPFS_API_ADD_URL = "https://ipfs.infura.io:5001/api/v0/add"
 IPFS_BASE_URL = "https://ipfs.io/ipfs/"
 
+# Remove output directory if exists. Else create one.
 if os.path.exists(OUTPUT_DIRECTORY):
     shutil.rmtree(OUTPUT_DIRECTORY)
     os.makedirs(OUTPUT_DIRECTORY)
@@ -35,9 +36,10 @@ with open(META_DATA_TEMPLATE_FILE) as f:
 
 df = pandas.read_csv(INPUT_FILE)
 
-image_list = df.values.tolist()
+image_list = df.to_dict(orient='records')
 for image in image_list:
-    image_url = image[4]
+    image["Instagram Username"] = image["Instagram ID"]
+    image_url = image["WasabiURL"]
     image_name =urllib.parse.urlparse(image_url).path.rstrip('/').split('/')[-1]
     image_path = OUTPUT_DIRECTORY + image_name
     r = requests.get(image_url)
@@ -49,27 +51,24 @@ for image in image_list:
     }
     response = requests.post(INFURA_IPFS_API_ADD_URL, files=image_files, auth=(INFURA_IPFS_PROJECT_ID,INFURA_IPFS_PROJECT_SECRET))
     ipfs_image_data = response.json()
-    image.append(ipfs_image_data["Hash"])
-    image.append(IPFS_BASE_URL + ipfs_image_data["Hash"])
+    image["Image IPFS CID"] = ipfs_image_data["Hash"]
+    image["Image IPFS URL"] = IPFS_BASE_URL + ipfs_image_data["Hash"]
 
+    # Generate metadata json from metadata template
     metadata_json = copy.deepcopy(json_template)
     metadata_json["name"] = image_name
     metadata_json["description"] = image_name
-    # 'name', 'description', 'external_url', 'instagram_username', 'instagram_share_url',
-    # 'instagram_direct_link', 'image_ipfs_cid', 'contract_address', 'vintage_date', 'token_id'
     metadata_json["external_url"] = IPFS_BASE_URL + ipfs_image_data["Hash"]
-    metadata_json["instagram_username"] = image[1]
-    metadata_json["instagram_share_url"] = image[2]
-    metadata_json["instagram_direct_link"] = image[3]
+    metadata_json["instagram_username"] = image["Instagram ID"]
+    metadata_json["instagram_share_url"] = image["Instagram Pic Share URL"]
+    metadata_json["instagram_direct_link"] = image["Instagram Direct Link"]
     metadata_json["image_ipfs_cid"] = ipfs_image_data["Hash"]
     metadata_json["contract_address"] = WALLET_ADDRESS
-    metadata_json["vintage_date"]=image[5]
-    metadata_json["token_id"] = image[0]
+    metadata_json["vintage_date"]=image["Vintage Date"]
+    metadata_json["token_id"] = image["ID"]
 
     metadata_json_file_name = Path(image_path).stem + ".json"
     metadata_json_file_path = OUTPUT_DIRECTORY + metadata_json_file_name
-    with open(metadata_json_file_path, 'w') as fp:
-        json.dump(metadata_json, fp, default=str)
     with open(metadata_json_file_path, 'w') as fp:
         json.dump(metadata_json, fp, default=str)
 
@@ -79,10 +78,12 @@ for image in image_list:
 
     response = requests.post(INFURA_IPFS_API_ADD_URL, files=json_files, auth=(INFURA_IPFS_PROJECT_ID,INFURA_IPFS_PROJECT_SECRET))
     ipfs_metadata_data = response.json()
-    image.append(ipfs_metadata_data["Hash"])
-    image.append(IPFS_BASE_URL + ipfs_metadata_data["Hash"])
-    image.append(WALLET_ADDRESS)
-    image.append(image[0])
+    image["Metadata CID"] = ipfs_metadata_data["Hash"]
+    image["Metadata IPFS URL"] = IPFS_BASE_URL + ipfs_metadata_data["Hash"]
+    image["Contract Address"] = WALLET_ADDRESS
+    image["Token ID"] = image["ID"]
+
+#Write image_list dictionary to csv
 colnames = ['ID',
  'Instagram Username',
  'Instagram Pic Share URL',
